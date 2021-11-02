0f5e8a2e6d2769f1c3d920842265263297885d94
RodrigoRVSN <rodrigovictor81@gmail.com>
2021-11-02 18:16:24 -0300

    🚀 procfile

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,12 @@ import express from "express";
 import { cors } from "./middlewares/cors";
 import { routes } from "./routes";
 
-const port = process.env.port || 3333;
-const infoPort = port === 3333 && "http://localhost:3333";
+const PORT = process.env.PORT || 3333;
+const infoPort = PORT === 3333 && "http://localhost:3333";
 
 const app = express();
 app.use(express.json());
 app.use(cors);
 app.use(routes);
 
-app.listen(port, () => console.log(`🔥 Running in ${infoPort}`));
+app.listen(PORT, () => console.log(`🔥 Running in ${infoPort}`));
